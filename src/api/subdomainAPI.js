436b0ee53bd95687ce81d450889083408bf4aef5
BugNode0x <sam@bugnode.io>
2024-01-23 18:11:04 -0500

    backend base url implemented

--- src/api/subdomainAPI.js
+++ src/api/subdomainAPI.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:3001';
+const BASE_URL = 'https://prototype-apidev.noisse.io/';
 
 // src/api/subdomainAPI.js
 export const enumerateSubdomains = async (domain) => {
