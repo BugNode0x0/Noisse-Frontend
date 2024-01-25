80c5f584a199ceb3118b1b9714a264da442194cb
BugNode0x <sam@bugnode.io>
2024-01-25 12:49:18 -0500

    fixes auth

--- src/api/subdomainAPI.js
+++ src/api/subdomainAPI.js
@@ -4,16 +4,7 @@ const BASE_URL = 'https://noisse-backend-production.up.railway.app';
 
 const api = axios.create({
   baseURL: BASE_URL,
-});
-
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-}, (error) => {
-  return Promise.reject(error);
+  withCredentials: true,
 });
 
 export const enumerateSubdomains = async (domain) => {
