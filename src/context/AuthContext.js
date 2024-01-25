576807b85ba55039901607326be618bf901b745d
sam <149985200+BugNode0x@users.noreply.github.com>
2024-01-25 18:30:59 -0500

    Update AuthContext.js

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,18 +1,27 @@
-import React, { createContext, useState, useContext } from 'react';
+import React, { createContext, useContext, useState, useEffect } from 'react';
+import axios from 'axios';
 
 const AuthContext = createContext(null);
 
 export const AuthProvider = ({ children }) => {
-  const [auth, setAuth] = useState({ token: localStorage.getItem('token'), isAuthenticated: false });
+  const [user, setUser] = useState(null);
 
-  // Call this function when you want to update the auth state globally
-  const setAuthToken = (token) => {
-    localStorage.setItem('token', token);
-    setAuth({ token, isAuthenticated: !!token });
-  };
+  useEffect(() => {
+    // Check auth status on initial load
+    const checkAuthStatus = async () => {
+      try {
+        const response = await axios.get('https://noisse-backend-production.up.railway.app/user', { withCredentials: true });
+        setUser(response.data.isAuthenticated ? response.data.user : null);
+      } catch (error) {
+        setUser(null);
+      }
+    };
+
+    checkAuthStatus();
+  }, []);
 
   return (
-    <AuthContext.Provider value={{ auth, setAuthToken }}>
+    <AuthContext.Provider value={{ user, setUser }}>
       {children}
     </AuthContext.Provider>
   );
