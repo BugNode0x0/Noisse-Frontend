361f4f27c874e4774dc287bfe0fc050e241639f7
sam <149985200+BugNode0x@users.noreply.github.com>
2024-01-26 12:21:02 -0500

    Update LogoutButton.js

--- src/components/LogoutButton.js
+++ src/components/LogoutButton.js
@@ -1,18 +1,21 @@
 import axios from 'axios';
 import React from 'react';
 import styles from './Header/User/User.module.sass'; 
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from "../context/AuthContext"
 
 
 
 const LogoutButton = ({ onLogout }) => {
   const { setUser } = useAuth();
-  const history = useHistory();
+  const navigate = useNavigate(); // Updated for React Router v6
+
   const handleLogout = async () => {
     try {
       // Call backend to clear the session
       await axios.get('https://noisse-backend-production.up.railway.app/logout', { withCredentials: true });
       setUser(null);
-      history.push('/sign-in');
+      navigate('/sign-in'); // Updated for React Router v6
     } catch (error) {
       console.error('Logout failed', error);
     }
