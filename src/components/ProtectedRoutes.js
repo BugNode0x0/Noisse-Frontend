ed7c32fb67e8d7baf9695db10786cf7b617a0efe
sam <149985200+BugNode0x@users.noreply.github.com>
2024-01-26 11:48:37 -0500

    Create ProtectedRoutes.js

---
+++ src/components/ProtectedRoutes.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Navigate, Outlet, useLocation } from 'react-router-dom';
+import { useAuth } from '../context/AuthContext';
+
+const ProtectedRoutes = () => {
+  const { user } = useAuth();
+  const location = useLocation();
+
+  if (!user) {
+    return <Navigate to="/sign-in" state={{ from: location }} replace />;
+  }
+
+  return <Outlet />;
+};
+
+export default ProtectedRoutes;
