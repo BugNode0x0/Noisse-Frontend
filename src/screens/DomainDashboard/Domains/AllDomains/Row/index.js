78962c9c67d933b63dd56cf205900e9ed8692812
sam <149985200+BugNode0x@users.noreply.github.com>
2024-01-29 10:06:52 -0500

    removing checkbox

--- src/screens/DomainDashboard/Domains/AllDomains/Row/index.js
+++ src/screens/DomainDashboard/Domains/AllDomains/Row/index.js
@@ -2,17 +2,11 @@
 
 import React from 'react';
 import styles from './Row.module.sass';
-import Checkbox from '../../../../../components/Checkbox';
 
 const Row = ({ item, value, onChange }) => {
   return (
     <div className={styles.row}>
       <div className={styles.col}>
-        <Checkbox
-          className={styles.checkbox}
-          value={value}
-          onChange={onChange}
-        />
       </div>
       <div className={styles.col}>
         {item} 
