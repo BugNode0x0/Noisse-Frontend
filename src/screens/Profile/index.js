449550afd51a8dfdb6ce75210b603056c10bd90a
sam <149985200+BugNode0x@users.noreply.github.com>
2024-01-30 17:47:16 -0500

    change?

--- src/screens/Profile/index.js
+++ src/screens/Profile/index.js
@@ -1,56 +1,19 @@
-import React, { useState } from "react";
+import React from "react";
 import styles from "./CustomerList.module.sass";
 import cn from "classnames";
 import Card from "../../components/Card";
-import Table from "./Table";
-import Panel from "./Panel";
 import Details from "./Details";
 
-
 const Profile = () => {
-  const [activeIndex, setActiveIndex] = useState(0);
-  const [search, setSearch] = useState("");
-  const [visible, setVisible] = useState(false);
-
   return (
-    <>
-      <Card
-        className={styles.card}
-        title="Customer"
-        classTitle={cn("title-purple", styles.title)}
-        classCardHead={cn(styles.head, { [styles.hidden]: visible })}
-        head={
-          <>
-            <div className={styles.nav}>
-              {navigation.map((x, index) => (
-                <button
-                  className={cn(styles.link, {
-                    [styles.active]: index === activeIndex,
-                  })}
-                  onClick={() => setActiveIndex(index)}
-                  key={index}
-                >
-                  {x}
-                </button>
-              ))}
-            </div>
-          </>
-        }
-      >
-        <div className={cn(styles.row, { [styles.flex]: visible })}>
-          <Table
-            className={styles.table}
-            activeTable={visible}
-            setActiveTable={setVisible}
-          />
-          <Details
-            className={styles.details}
-            onClose={() => setVisible(false)}
-          />
-        </div>
-      </Card>
-      <Panel />
-    </>
+    <Card
+      className={styles.card}
+      title="Customer"
+      classTitle={cn("title-purple", styles.title)}
+      classCardHead={styles.head}
+    >
+      <Details className={styles.details} />
+    </Card>
   );
 };
 
