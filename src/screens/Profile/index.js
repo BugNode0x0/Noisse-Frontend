7828308d05d267c06038f29db4439f3f04071e42
sam <149985200+BugNode0x@users.noreply.github.com>
2024-01-30 17:36:57 -0500

    continue mate

--- src/screens/Profile/index.js
+++ src/screens/Profile/index.js
@@ -2,9 +2,6 @@ import React, { useState } from "react";
 import styles from "./CustomerList.module.sass";
 import cn from "classnames";
 import Card from "../../components/Card";
-import Form from "../../components/Form";
-import Filters from "../../components/Filters";
-import Settings from "./Settings";
 import Table from "./Table";
 import Panel from "./Panel";
 import Details from "./Details";
@@ -29,16 +26,6 @@ const Profile = () => {
         classCardHead={cn(styles.head, { [styles.hidden]: visible })}
         head={
           <>
-            <Form
-              className={styles.form}
-              value={search}
-              setValue={setSearch}
-              onSubmit={() => handleSubmit()}
-              placeholder="Search by name or email"
-              type="text"
-              name="search"
-              icon="search"
-            />
             <div className={styles.nav}>
               {navigation.map((x, index) => (
                 <button
@@ -52,12 +39,6 @@ const Profile = () => {
                 </button>
               ))}
             </div>
-            <Filters
-              className={styles.filters}
-              title="Showing 10 of 24 customer"
-            >
-              <Settings />
-            </Filters>
           </>
         }
       >
