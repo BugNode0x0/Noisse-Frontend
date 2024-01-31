32e78e44ddcabf2f22362c573b18a2b0baad5f53
sam <149985200+BugNode0x@users.noreply.github.com>
2024-01-30 19:35:39 -0500

    continue implementing auth

--- src/components/Header/Notification/Item/index.js
+++ src/components/Header/Notification/Item/index.js
@@ -3,31 +3,12 @@ import { Link } from "react-router-dom";
 import cn from "classnames";
 import styles from "./Item.module.sass";
 
-const Item = ({ className, item, onClose }) => {
+const Item = ({ message, type }) => {
   return (
-    <Link
-      className={cn(styles.item, { [styles.new]: item.new }, className)}
-      to={item.url}
-      onClick={onClose}
-    >
-      <div className={styles.avatar}>
-        <img src={item.avatar} alt="Avatar" />
-        <div className={styles.icon} style={{ backgroundColor: item.color }}>
-          <img src={item.icon} alt="Status" />
-        </div>
-      </div>
-      <div className={styles.details}>
-        <div className={styles.line}>
-          <div className={styles.subtitle}>{item.title}</div>
-          <div className={styles.login}>{item.login}</div>
-          <div className={styles.time}>{item.time}</div>
-        </div>
-        <div
-          className={styles.content}
-          dangerouslySetInnerHTML={{ __html: item.content }}
-        ></div>
-      </div>
-    </Link>
+    <div className={styles.item}>
+      <Icon name={type === "start" ? "play-circle" : "check-circle"} size="24" />
+      <p className={styles.message}>{message}</p>
+    </div>
   );
 };
 
