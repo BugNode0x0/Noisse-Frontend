58555ef9d449707a02bc790e33ba5bb7cde46365
sam <149985200+BugNode0x@users.noreply.github.com>
2024-02-02 12:32:32 -0500

    prod ftw

--- src/api/subdomainAPI.js
+++ src/api/subdomainAPI.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://noisse-backend-production.up.railway.app';
+const BASE_URL = 'https://api.noisse.io';
 
 const api = axios.create({
   baseURL: BASE_URL,
