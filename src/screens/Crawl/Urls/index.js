ac62a0597f3cef1b7865010d68935e8a99e9f717
BugNode0x <sam@bugnode.io>
2024-02-13 20:52:20 -0500

    new implementation without Row

--- src/screens/Crawl/Urls/index.js
+++ src/screens/Crawl/Urls/index.js
@@ -4,26 +4,25 @@ import Card from "../../../components/Card";
 import Dropdown from "../../../components/Dropdown";
 
 const Urls = ({ subdomain, urls }) => {
-    const [visible, setVisible] = React.useState(false);
+  const [visible, setVisible] = useState(false);
 
-    const toggleDropdown = (e) => {
-      e.stopPropagation(); // Prevent event bubbling up to the card
-      setVisible(!visible);
-    };
+  const toggleDropdown = (e) => {
+    e.stopPropagation(); // Prevent event bubbling up to the card
+    setVisible(!visible);
+  };
 
-
-    return (
-        <Card className={styles.card}>
-            <div className={styles.card} onClick={toggleDropdown}>
-                {subdomain}
-            </div>
-            {visible && (
-                <Dropdown
-                    options={urls.map(url => ({ value: url, label: url }))}
-                />
-            )}
-        </Card>
-    );
+  return (
+      <Card className={styles.card} onClick={toggleDropdown}>
+          <div className={styles.cardHead}>
+              {subdomain}
+          </div>
+          {visible && (
+              <Dropdown
+                  options={urls.map(url => ({ value: url, label: url }))}
+              />
+          )}
+      </Card>
+  );
 };
 
 export default Urls;
