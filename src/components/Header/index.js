6c88a846d820bbe408848420d17fdc31d3468048
sam <149985200+BugNode0x@users.noreply.github.com>
2024-02-20 18:39:17 -0500

    some dance to forget

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -18,7 +18,7 @@ const Header = ({ onOpen }) => {
       <div className={styles.control} onClick={() => setVisible(false)}>
       <Link className={cn("button", styles.button)} to="/domains/add">
           <Icon name="add" size="24" />
-          <span>Enumerate Asset</span>
+          <span>Start Recon</span>
         </Link>
         <User className={styles.user} />
       </div>
