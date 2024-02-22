0114e6f15cc1b17310c9eb0d1c44bd1c875a7bac
BugNode0x <sam@bugnode.io>
2024-02-22 11:40:11 -0500

    adding default payment in checkout

--- src/api/subdomainAPI.js
+++ src/api/subdomainAPI.js
@@ -171,7 +171,7 @@ export const cancelUserSubscription = async (webhookUrl) => {
 
 export const createUserSubscription = async () => {
   try {
-    const response = await api.post('/create-subscription');
+    const response = await api.post('/finalize-subscription');
     return response.data;
   } catch (error) {
     console.error('Error creating subscription:', error);
@@ -181,7 +181,7 @@ export const createUserSubscription = async () => {
 
 export const createStripeCheckoutSession = async () => {
   try {
-    const response = await api.post('https://noisse-backend-development.up.railway.app/create-checkout-session');
+    const response = await api.post('/create-checkout-session');
     return response.data; 
   } catch (error) {
     console.error('Error creating Stripe checkout session:', error);
