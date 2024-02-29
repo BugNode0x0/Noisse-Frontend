a44f77e1505a02c0f2fc7cca3e3915143d77f9b3
BugNode0x <sam@bugnode.io>
2024-02-29 16:13:48 -0500

    pls fix boi

--- src/screens/Profile/index.js
+++ src/screens/Profile/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext} from "react";
 import cn from "classnames";
 import styles from "./Profile.module.sass";
 import Card from "../../components/Card";
