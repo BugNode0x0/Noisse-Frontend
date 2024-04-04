3a61939acbdf7864cf23791b847fe414ec2a25f3
BugNode0x <sam@bugnode.io>
2024-04-04 19:56:26 -0400

    little by little deleting rows

--- src/screens/Hurls/Urls/Row/index.js
+++ src/screens/Hurls/Urls/Row/index.js
@@ -3,16 +3,12 @@
 import React from 'react';
 import styles from './Row.module.sass';
 
-const Row = ({ url, title, statusCode, contentLength, webServer, technology,selected, onChange }) => {
+const Row = ({ url,selected, onChange }) => {
   return (
     <div className={styles.row}>
       <div className={styles.col}>
       </div>
       <div className={styles.col}>{url}</div>
-      <div className={styles.col}>{statusCode}</div>
-      <div className={styles.col}>{contentLength}</div>
-      <div className={styles.col}>{webServer}</div>
-      <div className={styles.col}>{technology}</div>
 
 
     </div>
